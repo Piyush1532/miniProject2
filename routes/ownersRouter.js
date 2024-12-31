d708ba64d20c275b36c4bd040a9371f009db5750
Piyush1532 <piyushadav503@gmail.com>
2024-12-31 13:39:17 +0530

    routes file

---
+++ routes/ownersRouter.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const ownerModel = require("../models/owner-model");
+
+// router.get("/", (req, res) => {
+//   res.send("owner  is working");
+// });
+
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async (req, res) => {
+let {fullname,email,password}=req.body
+
+   let owners=await ownerModel.find()
+   if (owners.length>0) {
+    return res.status(503).send("You Don't Have Permission to Create a new owner")
+   }
+
+  let createOwner= await ownerModel.create({
+    fullname,
+    email,
+    password,
+   })
+   res.send(createOwner).status(200)
+  });
+}
+
+
+router.get("/admin", (req, res) => {
+  res.render("createproducts")
+});
+
+module.exports = router;
