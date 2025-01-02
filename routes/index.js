59101857ebc4203762ba1ee82cca3ae35e451f68
Piyush1532 <piyushadav503@gmail.com>
2025-01-02 15:11:48 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const router=express.Router()
 const isLoggedin=require("../middleware/isLoggedin")
+const productModel=require("../models/product-model")
 
 router.get("/",(req,res)=>{
 let error= req.flash("error")
@@ -9,8 +10,9 @@ res.render("index",{error})
 
 })
 
-router.get("/shop",isLoggedin,function (req,res) {
-   res.render("shop") 
+router.get("/shop",isLoggedin,async function  (req,res) {
+   let products=await productModel.find()
+   res.render("shop",{products}) 
 })
 
 router.get("/logout",isLoggedin,function (req,res) {
